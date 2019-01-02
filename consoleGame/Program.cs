25cbe1e5f6e0b8354dfa18ba27095a16e434d7f1
Trey <treyw1850@gmail.com>
2019-01-02 10:18:56 -0500

    stays poped up

--- consoleGame/Program.cs
+++ consoleGame/Program.cs
@@ -1,15 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
+// Namespace
 namespace consoleGame
 {
+    // Main Class
     class Program
     {
+        // Entry Point
         static void Main(string[] args)
         {
+            Console.WriteLine("sauceFam!");
+            Console.ReadKey();
         }
     }
 }
